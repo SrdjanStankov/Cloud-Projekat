af6e403ac2fdada181a8c7e4961ba1d432ff4c15
Srdjan <srki.miki@gmail.com>
2019-04-13 19:06:42 +0200

    uradjeno 1. a)

---
+++ Compute/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Compute
+{
+    internal class Program
+    {
+        public const int numberOfContainers = 4;
+        public static int currentPortNum = 10000;
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Press to start process");
+            Console.ReadKey(true);
+
+            var containers = new List<Process>();
+
+            for (int i = 0; i < numberOfContainers; i++)
+            {
+                containers.Add(new Process());
+            }
+
+            foreach (Process containerProcess in containers)
+            {
+                containerProcess.StartInfo.FileName = "E:\\SRKI\\FTN\\Cloud\\Projekat\\Container\\bin\\Debug\\Container.exe";
+                containerProcess.StartInfo.Arguments = currentPortNum++.ToString();
+                containerProcess.Start();
+            }
+
+            Console.ReadKey(true);
+
+            foreach (Process item in containers)
+            {
+                if (!item.HasExited)
+                {
+                    item.Kill();
+                }
+            }
+
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Container/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Container
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Container started....");
+
+            if (!int.TryParse(args[0], out int res))
+            {
+                Environment.Exit(-500); // BAD input argument
+            }
+
+            Console.WriteLine(res);
+
+            Console.ReadKey(true);
+        }
+    }
+}
