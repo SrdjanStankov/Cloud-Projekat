518a24d2aa7080bfb0707a1c84cd49daa46465b0
Srdjan <srki.miki@gmail.com>
2019-04-16 00:35:01 +0200

    8

--- Container/Program.cs
+++ Container/Program.cs
@@ -4,17 +4,30 @@ namespace Container
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        private static void Main(string[] args)// args[0] == port servera
         {
             Console.WriteLine("Container started....");
 
-            if (!int.TryParse(args[0], out int res))
+            if (!int.TryParse(args[0], out int port))
             {
                 Environment.Exit(-500); // BAD input argument
             }
 
-            Console.WriteLine(res);
+            Console.WriteLine(port);
 
+            //try
+            //{
+            //    var host = new ServiceHost(typeof(Worker));
+            //    host.AddServiceEndpoint(typeof(IWorker), new NetTcpBinding(), $"net.tcp://localhost:{port}");
+            //    host.Open();
+            //    Console.ReadKey(true);
+            //    host.Close();
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine(e);
+            //    throw;
+            //}
             Console.ReadKey(true);
         }
     }
