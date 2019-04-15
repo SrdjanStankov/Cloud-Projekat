b701d258c10caec32e863bd2c5f4851caa057568
Srdjan <srki.miki@gmail.com>
2019-04-15 20:30:43 +0200

    dd

--- Compute/Program.cs
+++ Compute/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using static System.Console;
+using static System.Environment;
 
 namespace Compute
 {
@@ -8,26 +9,29 @@ namespace Compute
 
         private static void Main(string[] args)
         {
-            Console.WriteLine("Press to start process");
-            Console.ReadKey(true);
+            WriteLine("Press to start process");
+            ReadKey(true);
 
             if (!ContainerFactory.Instance.CreateContainers(ComputeConfigurationManipulator.LoadConfiguration() ?? -1))
             {
-                Console.WriteLine("Nevalidna konfiguracija");
-                Environment.Exit(-100);     // BAD configuration
+                WriteLine("Nevalidna konfiguracija");
+                Exit(-100);     // BAD configuration
             }
 
             ContainerFactory.Instance.StartContainers();
 
             new FileWatcher().StartWatching();  // periodicno proverava da li se na predefinisanoj lokaciji nalazi novi paket
 
-            Console.WriteLine("Press key to abort all processes");
-            Console.ReadKey(true);
+            // TODO: kopira dll iz paketa na lokaciju container-a
+
+
+            WriteLine("Press key to abort all processes");
+            ReadKey(true);
 
             ContainerFactory.Instance.KillLiveContainers();
 
-            Console.WriteLine("Press key to exit");
-            Console.ReadKey(true);
+            WriteLine("Press key to exit");
+            ReadKey(true);
         }
     }
 }
