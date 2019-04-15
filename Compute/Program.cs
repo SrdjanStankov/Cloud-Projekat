98888f97bb9ec73583200f25768cb4cbdc09ebdc
Srdjan <srki.miki@gmail.com>
2019-04-16 00:28:50 +0200

    4

--- Compute/Program.cs
+++ Compute/Program.cs
@@ -1,4 +1,5 @@
-﻿using static System.Console;
+﻿using System.IO;
+using static System.Console;
 using static System.Environment;
 
 namespace Compute
@@ -19,6 +20,7 @@ namespace Compute
             }
 
             new FileWatcher().StartWatching();  // periodicno proverava da li se na predefinisanoj lokaciji nalazi novi paket
+            CopyDllToContainers();
 
             ContainerFactory.Instance.StartContainers();
 
@@ -34,6 +36,18 @@ namespace Compute
 
             WriteLine("Press key to exit");
             ReadKey(true);
+        }
+        private static void CopyDllToContainers()
+        {
+            foreach (string item in Directory.GetFiles(ComputeConfigurationContainer.ContainerExePath.Replace("Container.exe", ""), "*.dll"))
+            {
+                File.Delete(item);
+            }
+            string sourceFileName = Directory.GetFiles(ComputeConfigurationContainer.ConfigLocation, "*.dll")[0];
+            string[] splited = sourceFileName.Split('\\');
+            string fileName = splited[splited.Length - 1];
+            string destinationFileName = ComputeConfigurationContainer.ContainerExePath.Replace("Container.exe", fileName);
+            File.Copy(sourceFileName, destinationFileName, true);
         }
     }
 }
